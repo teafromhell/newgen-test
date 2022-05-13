1567c8192d2a828e9064ba48518ca8a8dc179bea
Dmitriy <tefromhell@gmail.com>
2022-05-13 18:43:18 +0300

    first commit

---
+++ dest/index.js
@@ -0,0 +1,35 @@
+"use strict";
+let courses = [
+    { name: "Courses in England", prices: [0, 100] },
+    { name: "Courses in Germany", prices: [500, null] },
+    { name: "Courses in Italy", prices: [100, 200] },
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+];
+function requiredRange(range) {
+    let result = courses.filter((item) => {
+        if (range[1] === null)
+            range[1] = 9000;
+        if (item.prices[1] === null)
+            item.prices[1] = 9000;
+        if (range[0] === null)
+            range[0] = 0;
+        if (item.prices[0] === null)
+            item.prices[0] = 0;
+        item.weight = item.prices[1] - item.prices[0];
+        if (item.prices[1] >= range[0] && item.prices[0] <= range[1])
+            return item;
+    });
+    return result
+        .sort((a, b) => (a.prices[0] > b.prices[0] ? 1 : -1))
+        .sort((a, b) => {
+        if (a.prices[0] === b.prices[0])
+            return a.weight > b.weight ? 1 : -1;
+    });
+}
+console.log(JSON.stringify(requiredRange([null, 200])));
+console.log(JSON.stringify(requiredRange([100, 350])));
+console.log(JSON.stringify(requiredRange([200, null])));
